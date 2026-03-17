a4a6212493a93335b01cf9b1c18e4cc1a64624a7
Rama-chandra55 <rock6262712@gmail.com>
2026-03-17 20:25:47 +0530

    Implementing a logic for Product of Array Except Self using python
    Added a comment header for clarity.

---
+++ ProductOfArray.py
@@ -0,0 +1,17 @@
+# Product of Array Except Self
+
+def product_except_self(nums):
+    n = len(nums)
+    result = [1] * n
+    prefix = 1
+    for i in range(n):
+        result[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(n-1, -1, -1):
+        result[i] *= postfix
+        postfix *= nums[i]
+    return result
+nums = [1,2,3,4]
+print(product_except_self(nums))
+
