2f8503f87ca3e5e792ca274687135344224f36ca
Rama-chandra55 <rock6262712@gmail.com>
2026-03-25 21:20:27 +0530

    Added two approach to find the shortest path in unweighted graph in python
    Added an alternative BFS implementation using deque.

--- shortestPath.py
+++ shortestPath.py
@@ -18,3 +18,17 @@ graph = {
     4: [1]
 }
 bfs(graph, 0)
+
+# Another approach - too simple
+from collections import deque
+def bfs(graph, start):
+    visited = set([start])
+    queue = deque([(start, 0)])
+    while queue:
+        node, d = queue.popleft()
+        print(node, d)
+        for n in graph[node]:
+            if n not in visited:
+                visited.add(n)
+                queue.append((n, d+1))
+
