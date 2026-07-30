2cadbcc49c31eaace409a6ddf39dd7a4db3349e9
Ramachandra J <rock6262712@gmail.com>
2026-07-30 19:57:49 +0530

    Implement greedy approach for Leet Code 3014

---
+++ MinimumNumberOfPushesToTypeWordI.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumPushes(self, word):
+        n = len(word)
+        
+        if n <= 8:
+            return n
+        if n <= 16:
+            return 8 + (n - 8) * 2
+        if n <= 24:
+            return 8 + 16 + (n - 16) * 3
+            
+        return 8 + 16 + 24 + (n - 24) * 4
+        
