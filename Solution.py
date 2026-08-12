ae55fc3d25b7eefa1888a03b6e5e144d49580b0d
Ramachandra J <rock6262712@gmail.com>
2026-08-12 22:32:24 +0530

    Fix variable name bug by replacing count with freq dictionary

---
+++ Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        freq={}
+        l=max_len=0
+        for r in range(len(nums)):
+            freq[nums[r]] = freq.get(nums[r],0) + 1
+            while freq[nums[r]] > k:
+                freq[nums[l]] -= 1
+                l += 1
+            max_len = max(max_len, r-l+1)
+        return max_len
