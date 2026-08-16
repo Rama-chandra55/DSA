980eada357ef943fb10aec0323919c811cb2796f
Ramachandra J <rock6262712@gmail.com>
2026-08-16 18:21:09 +0530

    Implement optimal remainder counting strategy for Stone Game IX

---
+++ stone_game_ix.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def stoneGameIX(self, stones):
+        cnt = [0, 0, 0]
+        for s in stones:
+            cnt[s % 3] += 1
+
+        if cnt[0] % 2 == 0:
+
+            return cnt[1] >= 1 and cnt[2] >= 1
+ 
+        return abs(cnt[1] - cnt[2]) > 2
