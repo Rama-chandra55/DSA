c6014d75bc21e17290cb18ab6d89e687cfd5b74e
Ramachandra J <rock6262712@gmail.com>
2026-09-15 22:13:01 +0530

    feat: add solution for leetcode 2472 maximum number of non overlapping palindrome substrings

---
+++ maxpalindromes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
+        n = len(s)
+        ans = 0
+        i = 0
+        
+        while i < n:
+
+            if i + k <= n:
+                sub1 = s[i : i + k]
+                if sub1 == sub1[::-1]:
+                    ans += 1
+                    i += k  
+                    continue
+
+            if i + k + 1 <= n:
+                sub2 = s[i : i + k + 1]
+                if sub2 == sub2[::-1]:
+                    ans += 1
+                    i += k + 1  
+                    continue
+            i += 1
+            
+        return ans
