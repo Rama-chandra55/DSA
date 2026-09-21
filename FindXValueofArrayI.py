d90e70f621bb5928c3be32e47241bb09a62a88e7
Ramachandra J <rock6262712@gmail.com>
2026-09-21 06:44:32 +0530

    Add solution for find x value of array i

---
+++ FindXValueofArrayI.py
@@ -0,0 +1,23 @@
+class Solution:
+    def resultArray(self, nums: List[int], k: int) -> List[int]:
+        ans = [0] * k
+
+        dp = [0] * k
+        
+        for num in nums:
+            new_dp = [0] * k
+            num_mod = num % k
+
+            new_dp[num_mod] = 1
+
+            for r in range(k):
+                if dp[r] > 0:
+                    new_mod = (r * num_mod) % k
+                    new_dp[new_mod] += dp[r]
+
+            for r in range(k):
+                ans[r] += new_dp[r]
+
+            dp = new_dp
+            
+        return ans
